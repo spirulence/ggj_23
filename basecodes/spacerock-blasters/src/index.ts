ae7e81f65c0ad2b3396d35ed7b9af99fe952f39b
Cameron Seebach <cameron.seebach@gmail.com>
2022-12-31 19:55:55 +0000

    first commit

---
+++ basecodes/spacerock-blasters/src/index.ts
@@ -0,0 +1,25 @@
+import * as Phaser from "phaser";
+
+import MainMenu from "./scenes/menu-scene"
+import LevelOne from "./scenes/level1-scene";
+import LevelOneIntro from "./scenes/level1-intro";
+
+
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 80 }
+        }
+    },
+    fps: {
+        target: 20,
+    },
+    scene: [MainMenu, LevelOneIntro, LevelOne]
+};
+
+var game = new Phaser.Game(config);
---
+++ basecodes/spacerock-blasters/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({})
+  ]
+};
